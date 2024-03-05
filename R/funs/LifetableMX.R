0e4a8cd5e6bec56a604dea9279aa6a96d9ee7b63
Basellini <basellini@demogr.mpg.de>
2024-03-05 13:11:16 +0100

    uploading LT function

---
+++ R/funs/LifetableMX.R
@@ -0,0 +1,64 @@
+# ------------  FUNCTIONS to construct lifetable from MX column -------------------- #
+# Date: 08/03/2017
+# Authors: Giancarlo Camarda
+#          Ugofilippo Basellini
+#
+# ---------------------------------------------------------------------------------- #
+
+## function for constructing a classic (& rather general) lifetable
+lifetable.mx <- function(x, mx, sex="M", ax=NULL){
+  m <- length(x)
+  n <- c(diff(x), NA)
+  if(is.null(ax)){
+    ax <- rep(0,m)
+    if(x[1]!=0 | x[2]!=1){
+      ax <- n/2
+      ax[m] <- 1 / mx[m]
+    }else{    
+      if(sex=="F"){
+        if(mx[1]>=0.107){
+          ax[1] <- 0.350
+        }else{
+          ax[1] <- 0.053 + 2.800*mx[1]
+        }
+      }
+      if(sex=="M"){
+        if(mx[1]>=0.107){
+          ax[1] <- 0.330
+        }else{
+          ax[1] <- 0.045 + 2.684*mx[1]
+        }
+      }
+      ax[-1] <- n[-1]/2
+      ax[m] <- 1 / mx[m]
+    }
+  }
+  qx  <- n*mx / (1 + (n - ax) * mx)
+  qx[m] <- 1
+  px  <- 1-qx
+  lx  <- cumprod(c(1,px))*100000
+  dx  <- -diff(lx)
+  Lx  <- n*lx[-1] + ax*dx
+  lx <- lx[-(m+1)]
+  Lx[m] <- lx[m]/mx[m]
+  Lx[is.na(Lx)] <- 0 ## in case of NA values
+  Lx[is.infinite(Lx)] <- 0 ## in case of Inf values
+  Tx  <- rev(cumsum(rev(Lx)))
+  ex  <- Tx/lx
+  return.df <- data.frame(x, n, mx, ax, qx, px, lx, dx, Lx, Tx, ex)
+  return(return.df)
+}
+
+## function for constructing a classic (& rather general) lifetable
+e0.mx <- function(x, mx, sex="M", ax=NULL){
+  lt <- lifetable.mx(x,mx, sex,ax)
+  return.ex <- lt$ex[1]
+  return(return.ex)
+}
+
+
+
+
+
+
+
